40f7819d11dae4c1b33e8a470315d0ab2b1f0cfa
jhy-3 <2405578454@qq.com>
2026-05-23 10:22:33 +0800

    Disable environment proxies for weather seeders

--- scripts/runtime/global_weather_map_watcher.py
+++ scripts/runtime/global_weather_map_watcher.py
@@ -85,6 +85,7 @@ class GlobalWeatherMapWatcher:
         self.snapshot_store = SnapshotStore(snapshot_sqlite_path)
         self.seed_meta_store = SeedMetaStore(redis_client=self.redis_client, redis_prefix=self.redis_prefix, snapshot_store=self.snapshot_store)
         self.requests = requests.Session()
+        self.requests.trust_env = False
 
     def namespace(self) -> str:
         return global_weather_map_service.GLOBAL_WEATHER_MAP_SNAPSHOT_NAMESPACE
